c43f7aced7860b81177a21f96adc736619ede1f3
Tyler Pieterse <admin@adaptica.digital>
2024-07-21 14:49:49 +0200

    Updated and refined permissions for simulations, control and mediation

--- src/Wangle.Core/Authorization/PermissionNames.cs
+++ src/Wangle.Core/Authorization/PermissionNames.cs
@@ -11,5 +11,18 @@
         
         // SIMULATIONS
         public const string Pages_Simulations = "Pages.Simulations";
+        public const string Pages_Simulations_View = "Pages.Simulations.View";
+        
+        // CONTROL
+        public const string Pages_Simulations_Control = "Pages.Simulations.Control";
+        public const string Pages_Simulations_Control_Request = "Pages.Simulations.Control.Request";
+        public const string Pages_Simulations_Control_Assume = "Pages.Simulations.Control.Assume";
+        public const string Pages_Simulations_Control_Interact = "Pages.Simulations.Control.Interact";
+        
+        // MEDIATE
+        public const string Pages_Simulations_Mediate = "Pages.Simulations.Mediate";
+        public const string Pages_Simulations_Mediate_View = "Pages.Simulations.Mediate.View";
+        public const string Pages_Simulations_Mediate_Participant_Grant = "Pages.Simulations.Mediate.Participant.Grant";
+        public const string Pages_Simulations_Mediate_Participant_Evict = "Pages.Simulations.Mediate.Participant.Evict";
     }
 }
--- src/Wangle.Core/Authorization/WangleAuthorizationProvider.cs
+++ src/Wangle.Core/Authorization/WangleAuthorizationProvider.cs
@@ -16,7 +16,55 @@ namespace Wangle.Authorization
             context.CreatePermission(PermissionNames.Pages_Tenants, L("Tenants"), multiTenancySides: MultiTenancySides.Host);
             
             // SIMULATIONS
-            var virtualClassrooms = context.CreatePermission(PermissionNames.Pages_Simulations, L("PermissionSimulations"),
+            var simulations = context.CreatePermission(PermissionNames.Pages_Simulations, 
+                L("PermissionToSimulations"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulations.CreateChildPermission(PermissionNames.Pages_Simulations_View, 
+                L("PermissionToSimulationsView"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            // CONTROL
+            var simulationsControl = simulations.CreateChildPermission(PermissionNames.Pages_Simulations_Control, 
+                L("PermissionToSimulationsControl"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulationsControl.CreateChildPermission(PermissionNames.Pages_Simulations_Control_Request, 
+                L("PermissionToSimulationsControlRequest"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulationsControl.CreateChildPermission(PermissionNames.Pages_Simulations_Control_Assume, 
+                L("PermissionToSimulationsControlAssume"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulationsControl.CreateChildPermission(PermissionNames.Pages_Simulations_Control_Interact, 
+                L("PermissionToSimulationsControlInteract"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            // MEDIATION
+            var simulationsMediation = simulations.CreateChildPermission(PermissionNames.Pages_Simulations_Mediate, 
+                L("PermissionToSimulationsMediate"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulationsMediation.CreateChildPermission(PermissionNames.Pages_Simulations_Mediate_View, 
+                L("PermissionToSimulationsMediateView"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulationsMediation.CreateChildPermission(PermissionNames.Pages_Simulations_Mediate_Participant_Grant, 
+                L("PermissionToSimulationsMediateParticipantGrant"),
+                multiTenancySides: MultiTenancySides.Tenant,
+                featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
+            
+            simulationsMediation.CreateChildPermission(PermissionNames.Pages_Simulations_Mediate_Participant_Evict, 
+                L("PermissionToSimulationsMediateParticipantEvict"),
                 multiTenancySides: MultiTenancySides.Tenant,
                 featureDependency: new SimpleFeatureDependency(WangleFeatures.SimulationsFeature));
         }
